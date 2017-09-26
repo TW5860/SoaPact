233a1c30e633a61fdcc4de868f894fe2d875a5ff
Kim Fehrs <kfehrs@thoughtworks.com>
2017-09-26 14:22:35 +0200

    Initial commit

---
+++ gs-consuming-web-service/src/main/java/country/Application.java
@@ -0,0 +1,37 @@
+
+package country;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.spring.guides.gs_producing_web_service.GetCountryResponse;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.setProperty("org.apache.cxf.Logger", "org.apache.cxf.common.logging.Slf4jLogger");
+		System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
+		System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+		System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
+		System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
+		SpringApplication.run(Application.class);
+	}
+
+	@Bean
+	CommandLineRunner lookup(CountryClient countryClient) {
+		return args -> {
+			String country = "Spain";
+
+			if (args.length > 0) {
+				country = args[0];
+			}
+			GetCountryResponse response = countryClient.getCountry(country);
+			System.err.println(response.getCountry().getCapital());
+		};
+	}
+
+}
