b371225c82555340f091e3b7924790db3de44cc9
Kim Fehrs <kfehrs@thoughtworks.com>
2017-09-27 14:55:16 +0200

    Delete io folder from Repo

--- gs-consuming-web-service/io/spring/guides/gs_producing_web_service/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://spring.io/guides/gs-producing-web-service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package io.spring.guides.gs_producing_web_service;
