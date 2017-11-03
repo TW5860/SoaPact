bd00ba2276bc17f5ce39dbcacf5e836446dbb591
Martin Soto <donsoto@gmail.com>
2017-11-03 16:54:31 +0100

    Rename methods in test class
    ...so that their name makes sense at all.

--- gs-consuming-web-service/src/test/java/pact/utils/JSONConverterTest.java
+++ gs-consuming-web-service/src/test/java/pact/utils/JSONConverterTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 
 public class JSONConverterTest {
-	private String convertJSONToXML(String xml) throws Exception {
+	private String xmlToJSON(String xml) throws Exception {
 		Reader reader = new StringReader(xml);
 		StringWriter writer = new StringWriter();
 
@@ -19,7 +19,7 @@ public class JSONConverterTest {
 		return writer.toString();
 	}
 
-	private String convertJSONToXML(String xml, Configuration jsonConfig) throws Exception {
+	private String xmlToJSON(String xml, Configuration jsonConfig) throws Exception {
 		Reader reader = new StringReader(xml);
 		StringWriter writer = new StringWriter();
 
@@ -31,13 +31,13 @@ public class JSONConverterTest {
 	@Test
 	public void xmlToJSON_convertsSimpleXMLToJSON() throws Exception {
 		JSONAssert.assertEquals("{a: {b: \"xxx\", c: \"yyy\"}}",
-				convertJSONToXML("<a><b>xxx</b><c>yyy</c></a>"), true);
+				xmlToJSON("<a><b>xxx</b><c>yyy</c></a>"), true);
 	}
 	
 	@Test
 	public void xmlToJSON_convertsXMLWithNamespacesToJSON() throws Exception {
 		JSONAssert.assertEquals("{\"n1#a\": {b: \"xxx\", c: \"yyy\"}}",
-				convertJSONToXML("<n1:a xmlns:n1=\"http://ze/ns1\"><b>xxx</b><c>yyy</c></n1:a>"), true);
+				xmlToJSON("<n1:a xmlns:n1=\"http://ze/ns1\"><b>xxx</b><c>yyy</c></n1:a>"), true);
 	}
 
 	@Test
@@ -47,7 +47,7 @@ public class JSONConverterTest {
 		namespaces.put("http://ze/ns1", "NN1");
 
 		JSONAssert.assertEquals("{\"NN1#a\": {b: \"xxx\", c: \"yyy\"}}",
-				convertJSONToXML("<n1:a xmlns:n1=\"http://ze/ns1\"><b>xxx</b><c>yyy</c></n1:a>",
+				xmlToJSON("<n1:a xmlns:n1=\"http://ze/ns1\"><b>xxx</b><c>yyy</c></n1:a>",
 								 jsonConfig), true);
 	}
 }
