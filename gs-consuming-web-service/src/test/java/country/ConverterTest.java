d0a5a33e5659c770fc3f3a4f9cca5d016690903a
Kim Fehrs <kfehrs@thoughtworks.com>
2017-11-03 09:29:58 +0100

    Added FileUtil and example SOAP Calls

--- gs-consuming-web-service/src/test/java/country/ConverterTest.java
+++ gs-consuming-web-service/src/test/java/country/ConverterTest.java
@@ -1,16 +1,16 @@
 package country;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import java.net.URL;
 import java.nio.charset.Charset;
 
 import org.json.JSONML;
 import org.json.JSONObject;
-import org.json.XML;
 import org.junit.Test;
 
-import pact.utils.FileUtil;
+import pact.utils.FileUtils;
+import pact.utils.JSONConverter;
 import pact.utils.JSONMLConverter;
 
 public class ConverterTest {
@@ -29,9 +29,7 @@ public class ConverterTest {
 	
 	@Test
 	public void conversionToJSONAndBackToXMLShouldNotResultInChange() {
-		ClassLoader classLoader = getClass().getClassLoader();
-		String path = classLoader.getResource("ValidSoapRequest.xml").getPath();
-		String xml = FileUtil.readFile(path, Charset.defaultCharset());
+		String xml = FileUtils.readFile("ValidSoapRequest.xml", Charset.defaultCharset());
 		JSONObject jsonObject = JSONML.toJSONObject(xml);
 		String xmlConverted = JSONML.toString(jsonObject);
 		assertEquals(xml,xmlConverted);
@@ -39,9 +37,7 @@ public class ConverterTest {
 	
 	@Test
 	public void testResponseAsJSON() {
-		ClassLoader classLoader = getClass().getClassLoader();
-		String path = classLoader.getResource("ValidSoapResponse.xml").getPath();
-		String xml = FileUtil.readFile(path, Charset.defaultCharset());
+		String xml = FileUtils.readFile("ValidSoapResponse.xml", Charset.defaultCharset());
 		JSONObject jsonObject = JSONML.toJSONObject(xml);
 		assertEquals(jsonObject.toString(),"{\"xmlns:SOAP-ENV\":\"http://schemas.xmlsoap.org/soap/envelope/\",\"childNodes\":[{\"tagName\":\"SOAP-ENV:Header\"},{\"childNodes\":[{\"xmlns:ns2\":\"http://spring.io/guides/gs-producing-web-service\",\"childNodes\":[{\"childNodes\":[{\"childNodes\":[\"Spain\"],\"tagName\":\"ns2:name\"},{\"childNodes\":[46704314],\"tagName\":\"ns2:population\"},{\"childNodes\":[\"Madrid\"],\"tagName\":\"ns2:capital\"},{\"childNodes\":[\"EUR\"],\"tagName\":\"ns2:currency\"}],\"tagName\":\"ns2:country\"}],\"tagName\":\"ns2:getCountryResponse\"}],\"tagName\":\"SOAP-ENV:Body\"}],\"tagName\":\"SOAP-ENV:Envelope\"}");
 		
---
+++ gs-consuming-web-service/src/test/java/pact/utils/BadgerfishConverter.java
@@ -0,0 +1,67 @@
+//package pact.utils;
+//
+//import java.io.ByteArrayInputStream;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.StringWriter;
+//
+//import javax.xml.parsers.FactoryConfigurationError;
+//import javax.xml.stream.XMLInputFactory;
+//import javax.xml.stream.XMLStreamException;
+//import javax.xml.stream.XMLStreamReader;
+//import javax.xml.stream.XMLStreamWriter;
+//import javax.xml.transform.TransformerConfigurationException;
+//import javax.xml.transform.TransformerException;
+//import javax.xml.transform.TransformerFactory;
+//import javax.xml.transform.TransformerFactoryConfigurationError;
+//import javax.xml.transform.stax.StAXResult;
+//import javax.xml.transform.stax.StAXSource;
+//
+//import org.codehaus.jettison.badgerfish.BadgerFishDOMDocumentParser;
+//import org.codehaus.jettison.badgerfish.BadgerFishXMLStreamWriter;
+//import org.codehaus.jettison.mapped.MappedNamespaceConvention;
+//import org.w3c.dom.Document;
+//
+//import com.jayway.jsonpath.Configuration;
+//
+//import net.minidev.asm.DefaultConverter;
+//
+//public class BadgerfishConverter {
+//	public static String jsonToXml(String json) {
+//		return "";
+//	}
+//
+//	// private String toJSON(Element srcDOM) throws Exception {
+//	// ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//	// new BadgerFishDOMDocumentSerializer(baos).serialize(srcDOM);
+//	// return new String(baos.toByteArray());
+//	// }
+//
+//	private String toXML(String jsonStr) throws Exception {
+//		ByteArrayInputStream bais = new ByteArrayInputStream(jsonStr.getBytes());
+//		Document resDOM = new BadgerFishDOMDocumentParser().parse(bais);
+//		// return printNode(resDOM);
+//		return "";
+//	}
+//
+//	public static String xmlToJSON(InputStream inputStream) throws FactoryConfigurationError, XMLStreamException,
+//			TransformerFactoryConfigurationError, TransformerConfigurationException, TransformerException, IOException {
+//		XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(inputStream);
+//
+//		Configuration jsonConfig = new Configuration();
+//		DefaultConverter jsonConverter = new DefaultConverter();
+//		jsonConverter.setEnforce32BitInt(true);
+//		jsonConfig.setTypeConverter(jsonConverter);
+//		StringWriter strWriter = new StringWriter();
+//		MappedNamespaceConvention jsonConvention = new MappedNamespaceConvention(jsonConfig);
+//		XMLStreamWriter writer = new BadgerFishXMLStreamWriter(strWriter);
+//
+//		TransformerFactory.newInstance().newTransformer().transform(new StAXSource(reader), new StAXResult(writer));
+//
+//		writer.close();
+//		strWriter.close();
+//
+//		return strWriter.toString();
+//	}
+//
+//}
