055b22fa0cfafe0a1851401eb7d47f4663765ed1
Martin Soto <donsoto@gmail.com>
2017-11-28 16:35:39 +0100

    Test reverse proxy with a simple HTTP client
    As opposed to using the SOAP client.

--- gs-consuming-web-service/src/test/java/pact/utils/ReverseProxyTest.java
+++ gs-consuming-web-service/src/test/java/pact/utils/ReverseProxyTest.java
@@ -2,34 +2,44 @@ package pact.utils;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 import country.CountryConfiguration;
 import io.spring.guides.gs_producing_web_service.CountriesPort;
 import io.spring.guides.gs_producing_web_service.GetCountryRequest;
 import io.spring.guides.gs_producing_web_service.GetCountryResponse;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class ReverseProxyTest {
+	public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
 	@Test
-	public void shouldPassAnXMLResponseUnmodified() {
-		// PREPARE
+	public void shouldPassAnXMLResponseUnmodified() throws IOException {
+		// Prepare:
 
-		// MOCK WS
-		String staticResponse = FileReader.readFile("ValidSoapResponse.xml");
-		StaticBackendServer jsonSoapService = new StaticBackendServer("localhost", 9999, staticResponse);
-		jsonSoapService.start();
+		// End server.
+		String staticResponse = "Zis is ze responze!";
+		StaticBackendServer endServer = new StaticBackendServer("localhost", 9999, staticResponse);
+		endServer.start();
 
+		// HTTP client.
+		OkHttpClient client = new OkHttpClient();
+
+		// Act:
 		ReverseProxy proxy = new ReverseProxy("localhost", 8080, "http://localhost:9999");
 		proxy.start();
 		
-		// ACT
-		CountriesPort countriesPort = CountryConfiguration.getCountriesPort("http://localhost:8080");
-		GetCountryRequest request = new GetCountryRequest();
-		request.setName("Spain");
-		GetCountryResponse response = countriesPort.getCountry(request);
+		RequestBody body = RequestBody.create(JSON, "");
+		Request request = new Request.Builder().url("http://localhost:8080").post(body).build();
+		Response response = client.newCall(request).execute();
 		
-		// VERIFY
-		assertEquals(response.getCountry().getCapital(), "Madrid");
+		// Verify:
+		assertEquals(staticResponse, response.body().string());
 	}
 }
