e1556f92595affa0cce696072d6b34399ccbb100
kofl91 <kimfehrs@gmail.com>
2017-11-30 15:58:43 +0100

    Add V2 & V3 of the producing webservice

---
+++ producing-web-serviceV3-remove-fields/src/main/java/server/CountryRepository.java
@@ -0,0 +1,38 @@
+package server;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.spring.guides.gs_producing_web_service.Country;
+import io.spring.guides.gs_producing_web_service.Currency;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+@Component
+public class CountryRepository {
+	private static final Map<String, Country> countries = new HashMap<>();
+
+	@PostConstruct
+	public void initData() {
+		Country spain = new Country();
+
+		countries.put(spain.getName(), spain);
+
+		Country poland = new Country();
+		poland.setPopulation(38186860);
+
+		countries.put(poland.getName(), poland);
+
+		Country uk = new Country();
+		uk.setName("United Kingdom");
+		uk.setPopulation(63705000);
+
+		countries.put(uk.getName(), uk);
+	}
+
+	public Country findCountry(String name) {
+		Assert.notNull(name, "The country's name must not be null");
+		return countries.get(name);
+	}
+}
