f590cb75a8160b8017881a5be9b1e004d8505416
kofl91 <kimfehrs@gmail.com>
2017-12-02 15:28:52 +0100

    Extract JSONConfig into Constructor Soap2JsonProxy

--- gs-consuming-web-service/src/main/java/pact/utils/proxy/SOAPToJSON2WayReverseProxy.java
+++ gs-consuming-web-service/src/main/java/pact/utils/proxy/SOAPToJSON2WayReverseProxy.java
@@ -4,51 +4,42 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
-import javax.xml.stream.XMLStreamException;
-
 import org.apache.cxf.helpers.IOUtils;
-import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.mapped.Configuration;
 
 import pact.utils.converter.SOAPToJSONConverter;
 
 public class SOAPToJSON2WayReverseProxy extends ReverseProxy {
 
+	
+	
+	private Configuration jsonConfig;
+
 	public SOAPToJSON2WayReverseProxy(String backServerURL) {
 		super(backServerURL);
+		jsonConfig = SOAPToJSONConverter.makeDefaultJSONConfig();
+		Map<String, String> namespaces = jsonConfig.getXmlToJsonNamespaces();
+		namespaces.put("http://spring.io/guides/gs-producing-web-service", "");
 	}
 
 	@Override
 	protected String changeRequest(InputStream bodyInputStream) throws IOException {
-		Configuration jsonConfig = SOAPToJSONConverter.makeDefaultJSONConfig();
-		Map<String, String> namespaces = jsonConfig.getXmlToJsonNamespaces();
-		namespaces.put("http://spring.io/guides/gs-producing-web-service", "");
-
-		String soapRequestToJSON;
 		try {
 			String theString = IOUtils.toString(bodyInputStream, "UTF-8");
-			soapRequestToJSON = SOAPToJSONConverter.soapRequestToJSON(theString, jsonConfig);
+			return SOAPToJSONConverter.soapRequestToJSON(theString, jsonConfig);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-		return soapRequestToJSON;
 	}
 
 	@Override
 	protected String changeResponse(String bodyText) {
 		try {
-			Configuration jsonConfig = SOAPToJSONConverter.makeDefaultJSONConfig();
-			Map<String, String> namespaces = jsonConfig.getXmlToJsonNamespaces();
-			namespaces.put("http://spring.io/guides/gs-producing-web-service", "");
-			String converted = SOAPToJSONConverter.jsonToSoapResponse(bodyText,
+			return SOAPToJSONConverter.jsonToSoapResponse(bodyText,
 					jsonConfig);
-			System.out.println(converted);
-			return converted;
-		} catch (JSONException | XMLStreamException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
-		return bodyText;
 	}
 
 	public static void runTest(String backServerURL, TestCase testCase) {
