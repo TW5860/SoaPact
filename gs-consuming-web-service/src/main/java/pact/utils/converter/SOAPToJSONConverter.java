cb3fb3cc960fb0372ffa8e37fc993f30325c4290
Martin Soto <donsoto@gmail.com>
2017-12-15 21:42:54 +0100

    Remove obsolete methods

--- gs-consuming-web-service/src/main/java/pact/utils/converter/SOAPToJSONConverter.java
+++ gs-consuming-web-service/src/main/java/pact/utils/converter/SOAPToJSONConverter.java
@@ -79,34 +79,4 @@ public class SOAPToJSONConverter {
 		Configuration jsonConfig = makeJSONConfig(nsMap);
 		return JSONConverter.jsonToXML(jsonResponse.toString(), jsonConfig);
 	}
-	
-	
-	public static Configuration makeDefaultJSONConfig() {
-		Configuration jsonConfig = JSONConverter.makeDefaultJSONConfig();
-		Map<String, String> namespaces = jsonConfig.getXmlToJsonNamespaces();
-		namespaces.put("http://schemas.xmlsoap.org/soap/envelope/", "soap");
-		return jsonConfig;
-	}
-
-	public static String soapRequestToJSON(String soapRequestXML, Configuration jsonConfig) throws XMLStreamException, JSONException {
-		String jsonRequestText = JSONConverter.xmlToJSON(soapRequestXML, jsonConfig);
-		JSONObject jsonRequest = new JSONObject(jsonRequestText);
-		JSONObject payload = jsonRequest.getJSONObject("soap#Envelope").getJSONObject("soap#Body");
-		return payload.toString();
-	}
-
-	public static String jsonToSoapResponse(String jsonResponsePayloadText, Configuration jsonConfig) throws JSONException, XMLStreamException {
-		JSONObject jsonResponse = new JSONObject();
-
-		JSONObject envelope = new JSONObject();
-		jsonResponse.put("soap#Envelope", envelope);
-		
-		JSONObject header = new JSONObject();
-		envelope.put("soap#Header", header);
-
-		JSONObject body = new JSONObject(jsonResponsePayloadText);
-		envelope.put("soap#Body", body);
-
-		return JSONConverter.jsonToXML(jsonResponse.toString(), jsonConfig);
-	}
 }
