56b82052ca7ae6311454b5dc64cab687a271fbae
Martin Soto <donsoto@gmail.com>
2017-12-15 21:50:42 +0100

    Improve exception handling in reverse proxy

--- gs-consuming-web-service/src/main/java/pact/utils/proxy/SOAPToJSONReverseProxy.java
+++ gs-consuming-web-service/src/main/java/pact/utils/proxy/SOAPToJSONReverseProxy.java
@@ -3,10 +3,11 @@ package pact.utils.proxy;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.apache.cxf.helpers.IOUtils;
-import org.codehaus.jettison.mapped.Configuration;
+import javax.xml.stream.XMLStreamException;
 
-import pact.utils.converter.JSONConverter;
+import org.apache.cxf.helpers.IOUtils;
+import org.codehaus.jettison.json.JSONException;
+
 import pact.utils.converter.SOAPToJSONConverter;
 
 public class SOAPToJSONReverseProxy extends ReverseProxy {
@@ -18,22 +19,20 @@ public class SOAPToJSONReverseProxy extends ReverseProxy {
 	@Override
 	protected String changeRequest(InputStream bodyInputStream) throws IOException {
 		String bodyText = IOUtils.toString(bodyInputStream);
-		try {
-			return SOAPToJSONConverter.soapRequestToJSON(bodyText);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
+			try {
+				return SOAPToJSONConverter.soapRequestToJSON(bodyText);
+			} catch (XMLStreamException | JSONException e) {
+				throw new RuntimeException(e);
+			}
 	}
 
 	@Override
 	protected String changeResponse(String bodyText) {
-		try {
-			return SOAPToJSONConverter.jsonToSoapResponse(bodyText);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
+			try {
+				return SOAPToJSONConverter.jsonToSoapResponse(bodyText);
+			} catch (JSONException | XMLStreamException e) {
+				throw new RuntimeException(e);
+			}
 	}
 
 	public static void runTest(String backServerURL, TestCase testCase) {
