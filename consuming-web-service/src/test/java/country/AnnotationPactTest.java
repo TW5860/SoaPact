b83b32d9b08786400ee60000c707c2b054e9c742
kofl91 <kimfehrs@gmail.com>
2018-02-21 09:50:08 +0100

    Use Builder Pattern to create Service Objects

--- consuming-web-service/src/test/java/country/AnnotationPactTest.java
+++ consuming-web-service/src/test/java/country/AnnotationPactTest.java
@@ -1,60 +1,59 @@
 package country;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.IOException;
-
 import au.com.dius.pact.consumer.ConsumerPactTestMk2;
 import au.com.dius.pact.consumer.MockServer;
 import au.com.dius.pact.consumer.dsl.DslPart;
+import au.com.dius.pact.consumer.dsl.PactDslSoapBody;
 import au.com.dius.pact.consumer.dsl.PactDslSoapBody2;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.model.RequestResponsePact;
-import io.spring.guides.gs_producing_web_service.CountriesPort;
-import io.spring.guides.gs_producing_web_service.Country;
-import io.spring.guides.gs_producing_web_service.GetCountryRequest;
-import io.spring.guides.gs_producing_web_service.GetCountryResponse;
-import au.com.dius.pact.consumer.dsl.PactDslSoapBody;
 import au.com.dius.pact.soap.proxy.SOAPToJSONReverseProxy;
+import io.spring.guides.gs_producing_web_service.*;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
 
 public class AnnotationPactTest extends ConsumerPactTestMk2 {
 
-	@Override
-	protected String consumerName() {
-		return "Country-Consumer";
-	}
+    @Override
+    protected String consumerName() {
+        return "Country-Consumer";
+    }
 
-	@Override
-	protected String providerName() {
-		return "Country-Data-Provider";
-	}
+    @Override
+    protected String providerName() {
+        return "Country-Data-Provider";
+    }
 
-	@Override
-	protected RequestResponsePact createPact(PactDslWithProvider builder) {
-		DslPart requestForAnExistingCountry;
-		GetCountryRequest request = new GetCountryRequest();
-		request.setName("Spain");
-		requestForAnExistingCountry = new PactDslSoapBody()
-				.withNs("http://spring.io/guides/gs-producing-web-service")
-				.fromObject(request, GetCountryRequest.class);
+    @Override
+    protected RequestResponsePact createPact(PactDslWithProvider builder) {
+        ObjectFactory factory = new ObjectFactory();
+        DslPart requestForAnExistingCountry;
 
-		Country country = new Country();
-		country.setName("Spain");
-		country.setCapital("Madrid");
-		GetCountryResponse response = new GetCountryResponse();
-		response.setCountry(country);
+        GetCountryRequest request = factory.createGetCountryRequest().withName("Spain");
+        requestForAnExistingCountry = new PactDslSoapBody()
+                .withNs("http://spring.io/guides/gs-producing-web-service")
+                .fromObject(request, GetCountryRequest.class);
 
-		// From Object
+        GetCountryResponse response = factory.createGetCountryResponse()
+                .withCountry(factory.createCountry()
+                        .withName("Spain")
+                        .withCapital("Madrid")
+                        .withCurrency(Currency.EUR)
+                        .withPopulation(7000));
+
+        // From Object
 //		DslPart responseForAnExistingCountry = new PactDslSoapBody()
 //				.withNs("http://spring.io/guides/gs-producing-web-service")
 //				.fromObject(response, GetCountryResponse.class);
 
-		PactDslSoapBody2 responseForAnExistingCountry = new PactDslSoapBody2();
-		responseForAnExistingCountry.withNs("http://spring.io/guides/gs-producing-web-service")
-				.fromObject(response,GetCountryResponse.class);
+        PactDslSoapBody2 responseForAnExistingCountry = new PactDslSoapBody2();
+        responseForAnExistingCountry.withNs("http://spring.io/guides/gs-producing-web-service")
+                .fromObject(response, GetCountryResponse.class);
 
-		// As SoapBody Object
-		// TODO: Enable type matching without specific values like:	.numberType("population")
+        // As SoapBody Object
+        // TODO: Enable type matching without specific values like:	.numberType("population")
 //		responseForAnExistingCountry = new PactDslSoapBody()
 //				.withNs("http://spring.io/guides/gs-producing-web-service")
 //				.object("getCountryResponse")
@@ -65,28 +64,28 @@ public class AnnotationPactTest extends ConsumerPactTestMk2 {
 //			.closeObject()
 //        .close();
 
-		return builder.given("provider is available") // NOTE: Using provider states are optional, you can leave it out
-				.uponReceiving("A request for an existing country").path("/").method("POST")
-					.body(requestForAnExistingCountry)
-				.willRespondWith()
-					.status(200)
-					.body(responseForAnExistingCountry)
-				.toPact();
-	}
+        return builder.given("provider is available") // NOTE: Using provider states are optional, you can leave it out
+                .uponReceiving("A request for an existing country").path("/").method("POST")
+                .body(requestForAnExistingCountry)
+                .willRespondWith()
+                .status(200)
+                .body(responseForAnExistingCountry)
+                .toPact();
+    }
 
-	@Override
-	protected void runTest(MockServer mockServer) throws IOException {
-		SOAPToJSONReverseProxy.runTest(mockServer.getUrl(), p -> {
-			CountriesPort countriesPort = CountryConfiguration.getCountriesPort(p.getUrl());
-			GetCountryResponse country = countriesPort
-					.getCountry(getCounrtyRequestForAnExistingCountry());
-			assertEquals(country.getCountry().getName(), "Spain");
-		});
-	}
+    @Override
+    protected void runTest(MockServer mockServer) throws IOException {
+        SOAPToJSONReverseProxy.runTest(mockServer.getUrl(), p -> {
+            CountriesPort countriesPort = CountryConfiguration.getCountriesPort(p.getUrl());
+            GetCountryResponse country = countriesPort
+                    .getCountry(getCounrtyRequestForAnExistingCountry());
+            assertEquals(country.getCountry().getName(), "Spain");
+        });
+    }
 
-	private GetCountryRequest getCounrtyRequestForAnExistingCountry() {
-		GetCountryRequest getCountryRequest = new GetCountryRequest();
-		getCountryRequest.setName("Spain");
-		return getCountryRequest;
-	}
+    private GetCountryRequest getCounrtyRequestForAnExistingCountry() {
+        GetCountryRequest getCountryRequest = new GetCountryRequest();
+        getCountryRequest.setName("Spain");
+        return getCountryRequest;
+    }
 }
